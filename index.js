4fee5259870df26fae68f0359aefb045e2e8a0d9
cynthia <cynthialairumbe@gmail.com>
2025-06-05 22:35:54 +0300

    add  file

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// 1) multiply
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+
+// 2) random
+let random = Math.floor(Math.random() * 100) + 1; // any number > 0
+
+// 3) mod
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4; // 22 % 6 = 4
+
+// 4) max
+let max = Math.max(1, 4, 13, 20, 5, 10); // 20
